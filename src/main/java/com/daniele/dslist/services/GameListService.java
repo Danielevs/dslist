5e25af9ddaddffa625015ae8b08e0e76fbd6adb5
Daniele <danielevs012@gmail.com>
2024-11-18 11:13:41 -0400

    Atualização

--- src/main/java/com/daniele/dslist/services/GameListService.java
+++ src/main/java/com/daniele/dslist/services/GameListService.java
@@ -8,17 +8,35 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.daniele.dslist.dto.GameListDTO;
 import com.daniele.dslist.entities.GameList;
+import com.daniele.dslist.projections.GameMinProjection;
 import com.daniele.dslist.repositories.GameListRepository;
+import com.daniele.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
 	
 	@Autowired
 	private GameListRepository gameListRepository;
+	@Autowired
+	private GameRepository gameRepository;
+	
 	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll(){
 		List<GameList> result = gameListRepository.findAll();
 			return result.stream().map(x -> new GameListDTO(x)).toList();		
+	}
+	
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		List <GameMinProjection> list = gameRepository.searchByList(listId);
+		
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for (int i = min; i<= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(),i);
+		}
 	}
 }
